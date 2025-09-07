dc5deb19f07fc44a5a427610eb3d93da15ce89a4
stefania-25 <pontetstefania@gmail.com>
2025-09-07 00:12:10 -0300

    Update script.js

--- Ejercicio fetch y json/js/script.js
+++ Ejercicio fetch y json/js/script.js
@@ -19,3 +19,17 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+fetch(DATA_URL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error al cargar el archivo JSON');
+    }
+    return response.json();
+  })
+  .then(data => {
+    showData(data.students);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
